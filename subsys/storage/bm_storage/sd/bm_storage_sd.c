9e43942f517007469acb111f9146e80a9d03ebbe
Emanuele Di Santo <emdi@nordicsemi.no>
2026-01-12 10:52:15 +0100

    storage: bm_storage_sd: wrap storing operations to queue in a function
    Wrap storing of operations into the queue in a function.
    
    Signed-off-by: Emanuele Di Santo <emdi@nordicsemi.no>

--- subsys/storage/bm_storage/sd/bm_storage_sd.c
+++ subsys/storage/bm_storage/sd/bm_storage_sd.c
@@ -128,6 +128,18 @@ static bool queue_load_next(void)
 	return (bytes == sizeof(struct bm_storage_sd_op));
 }
 
+static bool queue_store(const struct bm_storage_sd_op *op)
+{
+	uint32_t bytes;
+	unsigned int key;
+
+	key = irq_lock();
+	bytes = ring_buf_put(&sd_fifo, (uint8_t *)op, sizeof(*op));
+	irq_unlock(key);
+
+	return (bytes == sizeof(*op));
+}
+
 static void queue_process(void)
 {
 	uint32_t ret;
@@ -249,15 +261,8 @@ int bm_storage_backend_read(const struct bm_storage *storage, uint32_t src, void
 int bm_storage_backend_write(const struct bm_storage *storage, uint32_t dest,
 			     const void *src, uint32_t len, void *ctx)
 {
-	uint32_t written;
-	unsigned int key;
-
-	/* SoftDevice expects this alignment. */
-	if (!is_aligned32((uint32_t)src) || !is_aligned32(dest)) {
-		return -EFAULT;
-	}
-
-	struct bm_storage_sd_op op = {
+	bool queued;
+	const struct bm_storage_sd_op op = {
 		.storage = storage,
 		.ctx = ctx,
 		.src = src,
@@ -265,16 +270,17 @@ int bm_storage_backend_write(const struct bm_storage *storage, uint32_t dest,
 		.dest = dest,
 	};
 
-	key = irq_lock();
-	written = ring_buf_put(&sd_fifo, (void *)&op, sizeof(struct bm_storage_sd_op));
-	irq_unlock(key);
+	/* SoftDevice expects this alignment. */
+	if (!is_aligned32((uint32_t)src) || !is_aligned32(dest)) {
+		return -EFAULT;
+	}
 
-	if (written != sizeof(struct bm_storage_sd_op)) {
+	queued = queue_store(&op);
+	if (!queued) {
 		return -EIO;
 	}
 
 	queue_start();
-
 	return 0;
 }
 
