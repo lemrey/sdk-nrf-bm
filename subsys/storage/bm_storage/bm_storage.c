35964e42a7bbb6564ea38664e44eb19248d9a4a7
Emanuele Di Santo <emdi@nordicsemi.no>
2026-01-13 10:03:31 +0100

    storage: bm_storage: remove checks on `nvm_info`
    The `nvm_info` field is set by `bm_storage` itself upon initialization.
    Do not check whether it has been set correctly, there is a dedicated
    field to keep track of whether initialization has happened or not.
    
    Signed-off-by: Emanuele Di Santo <emdi@nordicsemi.no>

--- subsys/storage/bm_storage/bm_storage.c
+++ subsys/storage/bm_storage/bm_storage.c
@@ -93,7 +93,7 @@ int bm_storage_read(const struct bm_storage *storage, uint32_t src, void *dest, 
 		return -EFAULT;
 	}
 
-	if (!storage->initialized || !storage->nvm_info) {
+	if (!storage->initialized) {
 		return -EPERM;
 	}
 
@@ -116,7 +116,7 @@ int bm_storage_write(const struct bm_storage *storage, uint32_t dest, const void
 		return -EFAULT;
 	}
 
-	if (!storage->initialized || !storage->nvm_info) {
+	if (!storage->initialized) {
 		return -EPERM;
 	}
 
@@ -138,7 +138,7 @@ int bm_storage_erase(const struct bm_storage *storage, uint32_t addr, uint32_t l
 		return -EFAULT;
 	}
 
-	if (!storage->initialized || !storage->nvm_info) {
+	if (!storage->initialized) {
 		return -EPERM;
 	}
 
